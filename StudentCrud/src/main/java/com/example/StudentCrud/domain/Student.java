fd988b95dc4999b638dee7279afdfb6c0c7062d3
AnushaPeta <81097908+AnushaPeta@users.noreply.github.com>
2021-05-03 15:08:35 +0530

    Add files via upload

---
+++ StudentCrud/src/main/java/com/example/StudentCrud/domain/Student.java
@@ -0,0 +1,43 @@
+package com.example.StudentCrud.domain;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+@Entity
+@Table(name="Students")
+public class Student {
+	
+	@Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+    private String studentname;
+    private String course;
+    private int fee;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getStudentname() {
+		return studentname;
+	}
+	public void setStudentname(String studentname) {
+		this.studentname = studentname;
+	}
+	public String getCourse() {
+		return course;
+	}
+	public void setCourse(String course) {
+		this.course = course;
+	}
+	public int getFee() {
+		return fee;
+	}
+	public void setFee(int fee) {
+		this.fee = fee;
+	}
+
+}
